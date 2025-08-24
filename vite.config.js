e4d1704e909be609aea14d5fbf1630271f15cd1e
Netra3090 <npatel2703@gmail.com>
2025-08-24 15:50:39 +0530

    all files getting copied in the dist file

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
   build: {
-    outDir: 'dist'
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        { src: 'html/**/*', dest: 'html' }, // copies all files & subfolders from html/
-        { src: 'css/**/*', dest: 'css' },   // copies all files & subfolders from css/
-        { src: 'js/**/*', dest: 'js' }      // copies all files & subfolders from js/
-      ]
-    })
-  ]
-})
+    outDir: 'dist', // default
+  }
+});
